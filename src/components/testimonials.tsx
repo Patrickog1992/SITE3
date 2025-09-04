07cd84306578bc05d7dc42c85b09adfa828c8522
Patrick Oliveira <patrickpneus2018@hotmail.com>
2025-09-04 00:31:59 +0000

    troque o Roberto A. pela Sandra F.

--- src/components/testimonials.tsx
+++ src/components/testimonials.tsx
@@ -31,18 +31,18 @@ const testimonials = [
     likes: 77,
   },
   {
-    name: "Sandra F.",
-    avatar: "https://i.imgur.com/bHTJFVo.png",
-    testimonial: "Resultados visíveis em poucas semanas. Minha autoestima melhorou muito. Recomendo a todas as minhas amigas que passam pelo mesmo problema.",
-    aiHint: "woman portrait happy",
-    likes: 82,
-  },
-  {
     name: "Roberto A.",
     avatar: "https://i.imgur.com/GJZpDHa.png",
     testimonial: "O melhor investimento que fiz para minha saúde. Simples, prático e com resultados que eu não esperava. Adeus, fungos!",
     aiHint: "man smiling portrait",
     likes: 65,
+  },
+  {
+    name: "Sandra F.",
+    avatar: "https://i.imgur.com/bHTJFVo.png",
+    testimonial: "Resultados visíveis em poucas semanas. Minha autoestima melhorou muito. Recomendo a todas as minhas amigas que passam pelo mesmo problema.",
+    aiHint: "woman portrait happy",
+    likes: 82,
   },
 ];
 
